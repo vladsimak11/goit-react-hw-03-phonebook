e7001c78ae7d3e68b450b11cc20e5bff1856e17a
Vladyslav Simak <simakv21@gmail.com>
2023-04-03 12:36:11 +0300

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -62,7 +62,9 @@ export class App extends Component  {
     const contacts = localStorage.getItem('contacts');
     const parsedContacts = JSON.parse(contacts);
 
-    this.setState({contacts : parsedContacts})
+    if(parsedContacts) {
+      this.setState({contacts: parsedContacts})
+    }
   }
 
   componentDidUpdate(prevProps, prevState) {
